5eb2ad5efb550474c9a076e2b61d105880cba3bf
Isaac Sofan <digimonataque@gmail.com>
2026-04-10 15:48:56 -0500

    feat(app): registrar todos los módulos en app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
+import { UsersModule } from './users/users.module';
+import { SalesModule } from './sales/sales.module';
+import { InvoicesModule } from './invoices/invoices.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -11,6 +18,13 @@ import { DatabaseModule } from './database/database.module';
       envFilePath: ['.env'],
     }),
     DatabaseModule,
+    AuthModule,
+    ProductsModule,
+    ClientsModule,
+    UsersModule,
+    SalesModule,
+    InvoicesModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
